854023b644c749fc73248e7965954b54209fcad6
nilam309 <x20145331@student.ncirl.ie>
2024-04-22 18:54:40 +0100

    #with complete button

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,117 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./styles.css";
+import { useState } from "react";
+import "./App.css";
+import Task from "./Task";
+export default function App() {
+  //to-di list-2
+  const [toDoList, setToDoList] = useState([]);
+  const [newTask, setNewTask] = useState("");
 
-function App() {
-  const [count, setCount] = useState(0)
+  const handleChange = (event) => {
+    setNewTask(event.target.value);
+  };
+
+  const addTask = () => {
+    const task = {
+      id: toDoList.length === 0 ? 1 : toDoList[toDoList.length - 1].id + 1,
+      taskName: newTask,
+      completed: false,
+    };
+    setToDoList([...toDoList, task]);
+  };
+  const deleteTask = (id) => {
+    //const newToDoList = toDoList.filter((task) => task !== taskName);
+    setToDoList(toDoList.filter((task) => task.id !== id));
+  };
+  const makeCompleted = (id) => {
+    setToDoList(
+      toDoList.map((task) => {
+        if (task.id === id) {
+          return { ...task, completed: true };
+        } else {
+          return task;
+        }
+      })
+    );
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App">
+      <h1>MyTo-Do List</h1>
+      <input type="text" onChange={handleChange} />
+      <button onClick={addTask}>Add Task here</button>
+      <div className="list">
+        {toDoList.map((task) => {
+          return (
+            <Task
+              taskName={task.taskName}
+              id={task.id}
+              completed={task.completed}
+              deleteTask={deleteTask}
+              makeCompleted={makeCompleted}
+            />
+          );
+        })}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
 
-export default App
+  // //To-do List-1
+  // const [todolist, setTodoList] = useState([]);
+  // const [newTask, setNewTask] = useState("");
+  // const handleChange = ({ target }) => {
+  //   setNewTask(target.value);
+  // };
+  // const addTask = () => {
+  //   //const newToDoList = [...todolist, newTask];
+  //   setTodoList([...todolist, newTask]);
+  // };
+  // return (
+  //   <div className="App">
+  //     <div className="addTask">
+  //       <input onChange={handleChange}></input>
+  //       <button onClick={addTask}>Add task</button>
+  //     </div>
+  //     <div className="list">
+  //       {todolist.map((task) => {
+  //         return <h1>{task}</h1>;
+  //       })}
+  //     </div>
+  //   </div>
+  // );
+
+  // const [newItem, setNewItem] = useState("");
+  // return (
+  //   <>
+  //     <form className="new-item-form">
+  //       <div className="form-row">
+  //         <label htmlFor="item">New Item</label>
+  //         <input
+  //           type="text"
+  //           id="item"
+  //           value={newItem}
+  //           onChange={(e) => setNewItem(e.target.value)}
+  //         ></input>
+  //       </div>
+  //       <button className="btn">Add</button>
+  //     </form>
+  //     <h1 className="header">Todo List</h1>
+  //     <ul className="list">
+  //       <li>
+  //         <label>
+  //           <input type="checkbox" />
+  //           Item 1
+  //         </label>
+  //         <button className="btn btn-danger">Delete</button>
+  //       </li>
+  //       <li>
+  //         <label>
+  //           <input type="checkbox" />
+  //           Item 2
+  //         </label>
+  //         <button className="btn btn-danger">Delete</button>
+  //       </li>
+  //     </ul>
+  //   </>
+  // );
+}
